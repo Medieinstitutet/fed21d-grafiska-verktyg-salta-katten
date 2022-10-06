7d41ab290f1e0749a11c7070b1e3ae6b08d3021a
Hektor-Byfors <hektor.byfors@hotmail.com>
2022-10-06 17:33:59 +0200

    mobile done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Menu button
 let dripBurger = document.querySelector(".dripBurger");
 let openMenu = document.querySelector(".openMenu");
 let topLayerClosed = document.querySelector(".topLayerClosed");
@@ -9,6 +10,7 @@ dripBurger.addEventListener("click", () => {
     topLayerOpen.classList.toggle("displayNone");
 });
 
+// Cookie disclaimer
 let cookieButton = document.querySelectorAll(".cookieButton");
 let cookieButtonText = document.querySelectorAll(".cookieButtonText");
 let cookieDisclaimer = document.querySelector(".cookieDisclaimer");
@@ -27,8 +29,49 @@ cookieButtonText.forEach(text => {
 
 let mobileMenuCLose = document.querySelectorAll(".mobileMenuClose");
 
+// Close menu
 mobileMenuCLose.forEach(item => {
     item.addEventListener("click", () => {
         openMenu.classList.add("displayNone");
     });
+});
+
+// Review carousel
+let lindsay = document.getElementById("lindsay");
+let mark = document.getElementById("mark");
+let elisabeth = document.getElementById("elisabeth");
+let leftPointer = document.getElementById("left");
+let rightPointer = document.getElementById("right");
+
+leftPointer.addEventListener("click", () => {
+    if(!lindsay.classList.contains("displayNone")) {
+        lindsay.classList.add("displayNone");
+        elisabeth.classList.remove("displayNone");
+    } else if(!mark.classList.contains("displayNone")) {
+        mark.classList.add("displayNone");
+        lindsay.classList.remove("displayNone");
+    } else if(!elisabeth.classList.contains("displayNone")) {
+        elisabeth.classList.add("displayNone");
+        mark.classList.remove("displayNone");
+    };
+});
+
+rightPointer.addEventListener("click", () => {
+    if(!lindsay.classList.contains("displayNone")) {
+        lindsay.classList.add("displayNone");
+        mark.classList.remove("displayNone");
+    } else if(!mark.classList.contains("displayNone")) {
+        mark.classList.add("displayNone");
+        elisabeth.classList.remove("displayNone");
+    } else if(!elisabeth.classList.contains("displayNone")) {
+        elisabeth.classList.add("displayNone");
+        lindsay.classList.remove("displayNone");
+    };
+});
+
+// Scroll to top
+let scrollToTopArrow = document.getElementById("scrollToTopArrow");
+
+scrollToTopArrow.addEventListener("click", () => {
+    document.body.scrollTop = 0;
 });
